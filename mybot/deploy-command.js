7a08cb2438b5f7e5e6e6e80f79779c4c36a61edf
김형준 <kimhyungjun@gimhyeongjun-ui-MacBookPro.local>
2022-12-27 01:34:11 +0900

    야이씨발

---
+++ mybot/deploy-command.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('야').setDescription('이씨빨이라고 대답'),
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('성공했습니다'))
+    .catch(console.error);
